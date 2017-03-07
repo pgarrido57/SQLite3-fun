5fca9fe16e8422f319bc2abbe243d2c2f79c2293
Peter Garrido <peter.garrido57@gmail.com>
2017-03-07 09:55:00 -0600

    Creating Database

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('example.sqlite');
+
+const dropEmployees = () => {
+  db.run(`DROP TABLE employees`);
+};
+// dropEmployees();
+
+db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, salary NUMBER(6,2))");
+
+// db.run(`INSERT INTO employees VALUES (1, "Ashley", "Irwin", 5)`);
